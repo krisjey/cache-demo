5a323f9e06957dbbcb7188f217b7b6ae5f350fe2
Kris jeong(Jeong KyeongSeok) <smufu@naver.com>
2026-05-04 22:24:36 +0900

    Add application entrypoint

---
+++ src/main/java/com/example/demo/CacheDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@EnableCaching
+@SpringBootApplication
+public class CacheDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CacheDemoApplication.class, args);
+    }
+}
