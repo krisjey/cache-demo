a230f86a28e3fa30c4e98e08f5ea11a77a70488a
Kris jeong(Jeong KyeongSeok) <smufu@naver.com>
2026-05-04 22:26:32 +0900

    Add Caffeine cache configuration

---
+++ src/main/java/com/example/demo/config/CacheConfig.java
@@ -0,0 +1,42 @@
+package com.example.demo.config;
+
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.caffeine.CaffeineCache;
+import org.springframework.cache.support.SimpleCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.Duration;
+import java.util.List;
+
+@Configuration
+public class CacheConfig {
+
+    public static final String LOCAL_PRODUCTS = "localProducts";
+    public static final String TTL_PRODUCTS = "ttlProducts";
+    public static final String SIZE_LIMIT_PRODUCTS = "sizeLimitProducts";
+    public static final String BAD_PRODUCT_SEARCH = "badProductSearch";
+    public static final String GOOD_PRODUCT_SEARCH = "goodProductSearch";
+
+    @Bean
+    public CacheManager cacheManager() {
+        SimpleCacheManager cacheManager = new SimpleCacheManager();
+        cacheManager.setCaches(List.of(
+                caffeineCache(LOCAL_PRODUCTS, Duration.ofMinutes(30), 1_000),
+                caffeineCache(TTL_PRODUCTS, Duration.ofSeconds(10), 1_000),
+                caffeineCache(SIZE_LIMIT_PRODUCTS, Duration.ofMinutes(30), 3),
+                caffeineCache(BAD_PRODUCT_SEARCH, Duration.ofMinutes(30), 1_000),
+                caffeineCache(GOOD_PRODUCT_SEARCH, Duration.ofMinutes(30), 1_000)
+        ));
+        return cacheManager;
+    }
+
+    private CaffeineCache caffeineCache(String name, Duration ttl, long maximumSize) {
+        return new CaffeineCache(name, Caffeine.newBuilder()
+                .recordStats()
+                .expireAfterWrite(ttl)
+                .maximumSize(maximumSize)
+                .build());
+    }
+}
