a649f013bab29864cf02ea9b0083e6a594d55bf1
Kris jeong(Jeong KyeongSeok) <smufu@naver.com>
2026-05-05 20:04:27 +0900

    Avoid transaction rollback propagation during sample data race

--- src/main/java/com/example/demo/support/DataInitializer.java
+++ src/main/java/com/example/demo/support/DataInitializer.java
@@ -9,7 +9,6 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Component;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -27,7 +26,6 @@ public class DataInitializer implements ApplicationRunner {
     }
 
     @Override
-    @Transactional
     public void run(ApplicationArguments args) {
         if (productRepository.count() > 0) {
             log.info("Sample products already exist. skip initialization.");
